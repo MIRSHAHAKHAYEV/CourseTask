41a62dac2b27e09db5d80b10640b44251bdc7789
MIRSHAHAKHAYEV <agayev.mirshah.yusif.2022@asoiu.edu.az>
2025-11-13 21:24:03 +0400

    methods that not used deleted

--- src/model/Admin.java
+++ src/model/Admin.java
@@ -13,19 +13,7 @@ public class Admin {
         this.password = password;
     }
 
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
     public String getPassword() {
         return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
